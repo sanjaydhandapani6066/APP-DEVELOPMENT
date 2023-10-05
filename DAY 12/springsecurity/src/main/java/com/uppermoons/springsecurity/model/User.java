38a206a88cf982a849f762b5045899e9bbf8e3b3
sanjaydhandapani6066 <104960799+sanjaydhandapani6066@users.noreply.github.com>
2023-10-05 10:31:43 +0530

    Add files via upload

---
+++ DAY 12/springsecurity/src/main/java/com/uppermoons/springsecurity/model/User.java
@@ -0,0 +1,75 @@
+package com.uppermoons.springsecurity.model;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.uppermoons.springsecurity.model.enumerated.Role;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="_users")
+public class User implements UserDetails{
+   
+	@Id
+	private Long id;
+	private String name;
+	private String email; 
+	private String password;
+	private Boolean isEnable;
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return List.of(new SimpleGrantedAuthority(role.name));
+	}
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+   
+}
---
+++ DAY 12/springsecurity/src/main/java/com/uppermoons/springsecurity/model/enumerated/Role.java
@@ -0,0 +1,7 @@
+package com.uppermoons.springsecurity.model.enumerated;
+
+public class Role {
+
+	public Object name;
+
+}
